ad8616d89576ca53cc97d516c466500954616cad
helalubo <alejandrodemoraiz@gmail.com>
2020-09-13 07:01:53 -0300

    Segundo commit, Solucion usando DropDownList

--- TPs.DeMoraiz.Alejandro/FormCalculadora/FormCalculadora.Designer.cs
+++ TPs.DeMoraiz.Alejandro/FormCalculadora/FormCalculadora.Designer.cs
@@ -94,6 +94,7 @@
             // cmbOperador
             // 
             this.cmbOperador.AllowDrop = true;
+            this.cmbOperador.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbOperador.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.cmbOperador.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbOperador.FormattingEnabled = true;
